ee5dfc992e74631e08c6b84f37fb5b88820a6b76
Deok Eun Kim <deokeunkim@Deokui-MacBookPro.local>
2019-12-06 03:21:53 +0900

    네비게이션바 나오는데 성공

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React from 'react';
+
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/Appbar';
 import Drawer from '@material-ui/core/Drawer';
 import MenuItem from '@material-ui/core/MenuItem';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+
 
 const styles = {
     root: {
@@ -16,18 +18,23 @@ const styles = {
     }
 }
 
-class AppSell extends React.Component {
+class AppShell extends React.Component {
+ 
+
     constructor(props) {
         super(props);
         this.state = {
             toggle: false
         };
     }
+ 
 
     handleDrawerToggle = () => this.setState({toggle: !this.state.toggle});
+
     render() {
         const { classes } = this.props;
         return (
+  
             <div className={classes.root}>
                 <AppBar position='static'>
                     <IconButton className = {classes.menuButton} color="inherit" onClick={this.handleDrawerToggle}>
@@ -40,8 +47,10 @@ class AppSell extends React.Component {
                 </Drawer>
 
             </div>
+ 
         )
     }
 }
 
 export default withStyles(styles)(AppShell);
+
